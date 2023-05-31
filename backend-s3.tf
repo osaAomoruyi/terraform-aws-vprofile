e89d0f2bdbbf6414ebd822a7d21c6673dfcf0444
osaAomoruyi <ao.omoruyi@gmail.com>
2023-05-31 14:12:09 +0100

    Key Pairs fix

--- backend-s3.tf
+++ backend-s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terra-vprofile-state20"
-    key = "terraform/backend"
+    key    = "terraform/backend"
     region = "us-east-1"
   }
 
--- keypairs.tf
+++ keypairs.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "vprofilekey" {
-  public_key = ""
-  key_name = file(var.PUB_KEY_PATH)
+  key_name = "vprofilekey"
+  public_key = file(var.PUB_KEY_PATH)
 }
